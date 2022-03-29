c7b66e7939665f052b40b587126cad7db84fe936
lvwei25 <1965751527@qq.com>
2022-03-29 17:19:59 +0800

    修复链接回调

--- net/Router.go
+++ net/Router.go
@@ -1,5 +1,7 @@
 package net
 
+import "github.com/lvwei25/well_tcp/log"
+
 //TODO	服务路由类
 type ServerRouter struct {
 }
@@ -22,25 +24,25 @@ type WellConnRouter interface {
 }
 
 func (this *ServerRouter) OnStart() {
-
+	log.NewLoger().Info("OnStart")
 }
 
 func (this *ServerRouter) OnClose() {
-
+	log.NewLoger().Info("OnClose")
 }
 
 func (this *ConnRouter) OnConnect(conn *WellConnection) {
-
+	log.NewLoger().Info("OnConnect")
 }
 
 func (this *ConnRouter) OnError(err error) {
-
+	log.NewLoger().Info("OnError")
 }
 
 func (this *ConnRouter) OnMessage(request *WellRequest) {
-
+	log.NewLoger().Info("OnMessage")
 }
 
 func (this *ConnRouter) OnClose(conn *WellConnection) {
-
+	log.NewLoger().Info("OnClose")
 }
