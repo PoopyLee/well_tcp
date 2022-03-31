924fbb3bd21467833e872d96fbf7353e7dca39fe
lvwei25 <1965751527@qq.com>
2022-03-31 12:04:34 +0800

    1.1.4更新log

--- log/log.go
+++ log/log.go
@@ -1,18 +1,16 @@
 package log
 
 import (
-	"fmt"
 	"github.com/fatih/color"
-	"os"
+	"log"
 	"runtime"
 	"time"
 )
 
 type Log struct {
 	prefix string
+	log.Logger
 }
-
-var file *os.File
 
 type Logging interface {
 	Println(a ...interface{})
@@ -25,46 +23,32 @@ type Logging interface {
 	SetLogFile(File string)
 }
 
-func init() {
-	file = nil
-}
 func (this *Log) Debug(a ...interface{}) {
-	if file != nil {
-		file.WriteString(fmt.Sprintln(this.prefix, "DEBUG", a))
-	}
 	color.New(color.FgGreen).Println(this.prefix, "DEBUG", a)
+	this.Println(this.prefix, "DEBUG", a)
 }
 
 func (this *Log) Info(a ...interface{}) {
-	if file != nil {
-		file.WriteString(fmt.Sprintln(this.prefix, "INFO", a))
-	}
 	color.New(color.FgBlue).Println(this.prefix, "INFO", a)
+	this.Println(this.prefix, "INFO", a)
 }
 func (this *Log) Warn(a ...interface{}) {
-	if file != nil {
-		file.WriteString(fmt.Sprintln(this.prefix, "WARN", a))
-	}
 	color.New(color.FgYellow).Println(this.prefix, "WARN", a)
+	this.Println(this.prefix, "WARN", a)
 }
 func (this *Log) Error(a ...interface{}) {
-	if file != nil {
-		file.WriteString(fmt.Sprintln(this.prefix, "ERROR", a))
-	}
+
 	color.New(color.FgRed).Println(this.prefix, "ERROR", a)
+	this.Println(this.prefix, "ERROR", a)
 }
 func (this *Log) Panic(a ...interface{}) {
-	if file != nil {
-		file.WriteString(fmt.Sprintln(this.prefix, "PANIC", a))
-	}
 	color.New(color.FgCyan).Println(this.prefix, "PANIC", a)
+	this.Println(this.prefix, "PANIC", a)
 }
 
 func (this *Log) Println(a ...interface{}) {
-	if file != nil {
-		file.WriteString(fmt.Sprintln(a))
-	}
 	color.New().Println(a)
+	this.Println(a)
 }
 
 func (this *Log) SetPrefix(Prefix string) {
@@ -72,11 +56,7 @@ func (this *Log) SetPrefix(Prefix string) {
 }
 
 func (this *Log) SetLogFile(File string) {
-	file2, err := os.Create(File)
-	if err != nil {
-		color.New(color.FgRed).Println(this.prefix, "ERROR", err)
-	}
-	file = file2
+	this.SetLogFile(File)
 }
 
 //获取函数名字
